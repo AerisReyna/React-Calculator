2f80fc374eea0da4d17d4f8ba24372dfc777e6b9
EmbyrAriel <arielkesteloot@gmail.com>
2020-09-23 22:59:16 -0400

    Added a button

--- app.jsx
+++ app.jsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Title extends React.Component {
+class Button extends React.Component {
+  render() {
+    const classes = "btn btn-dark " + this.props.btnType;
+    return (<button type="button" className="btn btn>{this.props.value}</button>)
+  }
+}
+
+class Titleg extends React.Component {
   render() {
     return (<h1 className="h2">{this.props.label}</h1>)
   }
 }
 
-class Title extends React.Component {
-  render() {
-    return (<h1 className="h2">{this.props.label}</h1>)
-  }
-}
-
-class Title extends React.Component {
+class Titleh extends React.Component {
   render() {
     return (<h1 className="h2">{this.props.label}</h1>)
   }
@@ -36,6 +37,7 @@ class Calculator extends React.Component {
     return  (
       <div className="container text-center">
         <Title label={this.props.title} />
+        <Button value="+" btnType="btn-input" />
       </div>
     )
   }
